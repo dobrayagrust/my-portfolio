ebd423c0ce5395b85ae2e8ff4fe10b8bacc08929
dobrayagrust <rzhavoe.vremya@gmail.com>
2021-10-09 13:21:22 +0300

    refatoring laoyt, create gh-pages

--- src/components/header/nav/Nav.js
+++ src/components/header/nav/Nav.js
@@ -4,14 +4,14 @@ import style from './Nav.module.css';
 export const Nav = () => {
     return (
         <div className={style.container}>
-                <ul className={style.sectionList}>
-                    <li className={style.item}><a href="">Homepage</a></li>
-                    <li className={style.item}><a href="">About</a></li>
-                    <li className={style.item}><a href="">Skills</a></li>
-                    <li className={style.item}><a href="">Projects</a></li>
-                    <li className={style.item}><a href="">Contacts</a></li>
-                </ul>
+            <ul className={style.linkContainer}>
+                <li className={style.link}><a href="">Homepage</a></li>
+                <li className={style.link}><a href="">About</a></li>
+                <li className={style.link}><a href="">Skills</a></li>
+                <li className={style.link}><a href="">Projects</a></li>
+                <li className={style.link}><a href="">Contacts</a></li>
+            </ul>
         </div>
-    );
+);
 }
 
